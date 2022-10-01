b545958aaf0ea4482cb4a082a12f0914e8a02bc7
areeves1992 <areeves1992@gmail.com>
2022-10-01 10:43:28 -0400

    Function creation
    Create the function, download and append the new data

--- Code/Folly_The_Show_Code.R
+++ Code/Folly_The_Show_Code.R
@@ -16,6 +16,37 @@ library(data.table)
 ######### END OF INSTALL PACKAGES ##########
 ############################################
 
+###########################################
+######### FUNCTION DECLARATIONS ###########
+
+
+fetchData <- function(yearOfData, monthOfData){
+  
+  fileURL <- paste0('https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_',yearOfData,'_', monthOfData, '.zip')
+  
+  #fileURL <- 'https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2022_4.zip'
+  
+  download.file(fileURL, temp)
+  
+  unzip(temp, exdir = paste0(getwd(),"\\Data"))
+  
+  fileToReadIn <- paste0(paste0(getwd(),"\\Data") ,"\\", list.files(paste0(getwd(),"\\Data"), pattern = "On_"))
+  
+  flightDataTemp <- fread(fileToReadIn)
+  
+  file.remove(fileToReadIn)
+  
+  fwrite(flightDataTemp, paste0(paste0(getwd(),"\\Data") ,"\\","flightData.csv"), append = TRUE)
+  
+}#end of function fetchData
+
+
+
+######END OFFUNCTION DECLARATIONS #########
+###########################################
+
+
+
 #You will have to change this to make it work on your machine.
 workingDir <- 'C:\\Users\\areev\\MSDS455TheShow'
 
@@ -25,14 +56,8 @@ temp <- tempfile()
 
 #gui version: https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGJ&QO_fu146_anzr=b0-gvzr
 
-fileURL <- paste0('https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_','2022','_', '4', '.zip')
+yearOfData <- 1988
 
-#fileURL <- 'https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2022_4.zip'
+monthOfData <- 1
 
-download.file(fileURL, temp)
 
-unzip(temp, exdir = paste0(getwd(),"\\Data"))
-
-fileToReadIn <- paste0(paste0(getwd(),"\\Data") ,"\\", list.files(paste0(getwd(),"\\Data"), pattern = "On_"))
-
-flightData <- fread(fileToReadIn)
