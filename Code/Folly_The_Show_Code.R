42121aadbeba8ceafe058ce52e8342b5dc793bee
areeves1992 <areeves1992@gmail.com>
2022-10-01 10:27:49 -0400

    Update Folly_The_Show_Code.R
    - packes
    - cut the url up for looping

--- Code/Folly_The_Show_Code.R
+++ Code/Folly_The_Show_Code.R
@@ -1,6 +1,21 @@
 #Cool intro comments go here but I'll come back to those.
 
 
+###########################################
+######### INSTALL PACKAGES ################
+#Uncomment the section below if you need the packages
+#or if your code errors out.
+
+#install.packages("data.table")
+
+####
+
+library(data.table)
+
+
+######### END OF INSTALL PACKAGES ##########
+############################################
+
 #You will have to change this to make it work on your machine.
 workingDir <- 'C:\\Users\\areev\\MSDS455TheShow'
 
@@ -8,8 +23,16 @@ setwd(workingDir)
 
 temp <- tempfile()
 
-fileURL <- 'https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2022_4.zip'
+#gui version: https://www.transtats.bts.gov/DL_SelectFields.aspx?gnoyr_VQ=FGJ&QO_fu146_anzr=b0-gvzr
+
+fileURL <- paste0('https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_','2022','_', '4', '.zip')
+
+#fileURL <- 'https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_2022_4.zip'
 
 download.file(fileURL, temp)
 
 unzip(temp, exdir = paste0(getwd(),"\\Data"))
+
+fileToReadIn <- paste0(paste0(getwd(),"\\Data") ,"\\", list.files(paste0(getwd(),"\\Data"), pattern = "On_"))
+
+flightData <- fread(fileToReadIn)
