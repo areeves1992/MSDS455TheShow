445556e6eb8df738c2f0fc8245e302b0ab97b3d1
areeves1992 <areeves1992@gmail.com>
2022-10-28 21:34:13 -0400

    Update Folly_Airline_Study.R
    Have the look up tables joined in.

--- Code/Folly_Airline_Study.R
+++ Code/Folly_Airline_Study.R
@@ -18,11 +18,32 @@ library(dplyr)
 #Read in that data #########################
 ############################################
 
-setwd("G:\\My Drive\\Northwestern\\455 Data Viz\\The Show\\Data")
+setwd("G:\\My Drive\\Northwestern\\455 Data Viz\\The Show\\Data\\Airline Data")
+
+ptm <- proc.time()
+
+flightData <- data.table::fread("flightData2.csv")
+
+testFlightData <- head(flightData)
+
+cancellationReasons <- data.table::fread("L_CANCELLATION.csv")
 
 
-
-#flightData <- data.table::fread("grep 2020 flightData.csv")
+carrierCodes <- data.table::fread("L_UNIQUE_CARRIERS.csv")
 
 proc.time() - ptm
+
+#Clean the look up tables
+cancellationReasons <- cancellationReasons %>% rename(CancellationReasonDescription = Description)
+
+carrierCodes <- carrierCodes %>% rename(AirlineCarrier = Description)
+
+
+#Join the look up tables
+flightData <- left_join(flightData, carrierCodes, by = c("Reporting_Airline" = "Code"))
+
+flightData <- left_join(flightData, cancellationReasons, by = c("CancellationCode" = "Code"))
+
+
+testFlightData <- head(flightData, n = 100000)
 
